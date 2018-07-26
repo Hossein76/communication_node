635f07ee7293d17cb15e07a460eb6febb599a715
Hossein76 <hoseingohari76@gmail.com>
2018-07-26 08:43:21 +0430

    changes before test pack3 for map1 full communication

--- src/communication_node/scripts/message_handler.py
+++ src/communication_node/scripts/message_handler.py
@@ -83,14 +83,14 @@ class message_handle:
             # TODO prop_model = data.prop_model
             if(self.tag!="map"and self.tag!="Odom"):
                 print("new "+self.tag+" received")
-            robots_list=rospy.get_param("/robots_list")
-            if ((data.source not in robots_list )or(data.destination not in robots_list) ):
-                return;
-            connection_list=[];
-            connection_list=(rospy.get_param("/connection_list_"+data.source));
-            source_index=robots_list.index(data.destination);
-            if (connection_list[1+source_index]==1):
-            #if (True):
+            # robots_list=rospy.get_param("/robots_list")
+            # if ((data.source not in robots_list )or(data.destination not in robots_list) ):
+            #     return;
+            # connection_list=[];
+            # connection_list=(rospy.get_param("/connection_list_"+data.source));
+            # source_index=robots_list.index(data.destination);
+            # if (connection_list[1+source_index]==1):
+            if (True):
                     if (self.alt_type!=None):
                        self.message_publisher = rospy.Publisher(data.source +"/g_map", self.alt_type, queue_size=10)
                        i=0
